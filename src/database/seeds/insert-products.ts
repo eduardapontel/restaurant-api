4198d836f4e676a92f1cbae8355585858966941b
Eduarda Alves Pontel <109531518+eduardapontel@users.noreply.github.com>
2025-08-15 12:35:57 +0100

    translate menu items

--- src/database/seeds/insert-products.ts
+++ src/database/seeds/insert-products.ts
@@ -4,15 +4,15 @@ export async function seed(knex: Knex): Promise<void> {
     await knex('products').del();
 
     await knex('products').insert([
-        { name: 'Nhoque quatro queijos', price: 45 },
-        { name: 'Isca de frango', price: 60 },
-        { name: 'Tilápia com alcaparras', price: 100 },
-        { name: 'Escondidinho de carne', price: 75 },
-        { name: 'Porção de batatas fritas', price: 65 },
-        { name: 'Executivo de frango grelhado', price: 40 },
-        { name: 'Executivo de tilápia grelhada', price: 39 },
-        { name: 'Caldo de palmito', price: 30 },
-        { name: 'Refrigerante 350ml', price: 7.5 },
-        { name: 'Suco de laranja 440ml', price: 10 },
+        { name: 'Four-cheese gnocchi', price: 45 },
+        { name: 'Chicken strips', price: 60 },
+        { name: 'Tilapia with capers', price: 100 },
+        { name: 'Shredded beef casserole', price: 75 },
+        { name: 'French fries portion', price: 65 },
+        { name: 'Grilled chicken executive meal', price: 40 },
+        { name: 'Grilled tilapia executive meal', price: 39 },
+        { name: 'Hearts of palm soup', price: 30 },
+        { name: 'Soda 350ml', price: 7.5 },
+        { name: 'Orange juice 440ml', price: 10 },
     ]);
 }
